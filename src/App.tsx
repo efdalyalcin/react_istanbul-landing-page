16c0fe8377e5ef7efd665d6cec8f2114d942fc02
efdalyalcin <efdalyalcin@hotmail.com>
2022-08-07 19:11:28 +0300

    section three is also done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import Header from './components/Header/Header';
 import LiveWorkTogether from './components/LiveWorkTogether/LiveWorkTogether';
 import SectionOne from './components/SectionOne/SectionOne';
+import SectionThree from './components/SectionThree/SectionThree';
 import SectionTwo from './components/SectionTwo/SectionTwo';
 import SeperationBar from './components/SeperationBar/SeperationBar';
 
@@ -16,7 +17,12 @@ function App() {
         <SectionOne />
         <LiveWorkTogether />
         <SectionTwo />
+        <SectionThree />
       </main>
+      <div className="App-bosluk"></div>
+      {/* dont forget to remove bosluk from app.scss 
+        make the map clickable and sent it to googlemaps
+      */}
     </div>
   );
 }
